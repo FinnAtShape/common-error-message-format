13a3f61f60293dcbaec00624a96b28ea355ee05b
Finn Nielsen <finn@shapegames.com>
2021-04-29 15:36:00 +0200

    slightly more fleshed out with the jackson annotations

--- src/main/kotlin/com/shapegames/common/errors/model/ErrorMessages.kt
+++ src/main/kotlin/com/shapegames/common/errors/model/ErrorMessages.kt
@@ -1,5 +1,9 @@
 package com.shapegames.common.errors.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id
@@ -8,13 +12,37 @@ import java.time.ZoneOffset
 
 private fun nowInUtc(): OffsetDateTime = OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC)
 
+/**
+ * Base class for error messages.
+ */
 @JsonTypeInfo(use = Id.NAME, include = As.PROPERTY, property = "type")
+@JsonSubTypes(
+    value = [
+        Type(value = EmptyErrorMessage::class, name = "empty"),
+        Type(value = SimpleTextErrorMessage::class, name = "simple_message")
+    ]
+)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 sealed class BaseErrorMessage {
-    abstract val timestamp: OffsetDateTime
-    abstract val errorCode: String
+    @get:JsonProperty("timestamp") abstract val timestamp: OffsetDateTime
+    @get:JsonProperty("error_code") abstract val errorCode: String
 }
 
-data class SimpleErrorMessage(
+/**
+ * When the is no additional error information besides the error code.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class EmptyErrorMessage(
     override val timestamp: OffsetDateTime = nowInUtc(),
     override val errorCode: String
+) : BaseErrorMessage()
+
+/**
+ * A simple message.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class SimpleTextErrorMessage(
+    override val timestamp: OffsetDateTime = nowInUtc(),
+    override val errorCode: String,
+    @field:JsonProperty("message") val message: String
 ) : BaseErrorMessage()
